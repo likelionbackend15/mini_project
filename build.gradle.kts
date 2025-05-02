43b7302351e0de21752b2b8b9f4235e081431ed6
rktclgh <songchih@icloud.com>
2025-05-02 14:56:59 +0900

    build.gradle 버전 수정
    build.gradle 버전 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ subprojects {
 
     dependencies {
         implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-        implementation("mysql:mysql-connector-java:8.1.0")
+        implementation("mysql:mysql-connector-java:8.0.33")
         testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
         implementation("org.slf4j:slf4j-api:2.0.12")
         runtimeOnly("ch.qos.logback:logback-classic:1.4.12")

70afccad1beb8494d05e71bff78197f34bd16012
rktclgh <songchih@icloud.com>
2025-05-02 18:54:44 +0900

    build.gradle 수정
    꼭 환경에 맞게 수정하세요!

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,4 +1,9 @@
-plugins { java }
+plugins{
+    application
+    java
+
+}
+
 
 dependencies {
     // 공용(common) 모듈을 서버(server) 모듈에서 사용하겠다
@@ -9,3 +14,10 @@ dependencies {
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     implementation("org.mindrot:jbcrypt:0.4")   // bcrypt 해시 검증
 }
+application{
+    mainClass.set("com.studybuddy.server.ServerMain")
+}
+
+
+
+
